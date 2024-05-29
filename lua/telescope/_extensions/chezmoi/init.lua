0ea11d7ab4e9ce0b54612d60b562cd6bed44ed43
Young, Gianni <cmancy@tutanota.com>
2024-05-29 18:59:00 -0500

    added chez moi update,apply and add commands

--- lua/telescope/_extensions/chezmoi/init.lua
+++ lua/telescope/_extensions/chezmoi/init.lua
@@ -1,5 +1,21 @@
 local telescope = require("telescope")
 local chezmoi = require("telescope._extensions.chezmoi.chezmoi")
+local current_file = vim.fn.expand("%:p")
+
+vim.api.nvim_create_user_command("ChezmoiAddCurrentFile", function()
+	vim.fn.system("chezmoi add " .. current_file)
+	print("Added to chezmoi: " .. current_file)
+end, {})
+
+vim.api.nvim_create_user_command("ChezmoiUpdate", function()
+	vim.fn.system("chezmoi update")
+	print("Pulling Chezmoi Latest Configuration!")
+end, {})
+
+vim.api.nvim_create_user_command("ChezmoiApply", function()
+	vim.fn.system()
+	print("Applying Chezmoi Config!")
+end, {})
 
 return telescope.register_extension({
 	exports = {
